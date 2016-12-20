761e1999e600d8944771ca7bae66bba31e47af39
Kunio Sawatani <devel@fathens.org>
2016-12-20 11:24:42 +0900

    PluginGradle を汎用化

--- gemlib/cordova_plugin_kotlin.rb
+++ gemlib/cordova_plugin_kotlin.rb
@@ -1,23 +1,18 @@
 require 'pathname'
 
 class PluginGradle
-    def self.with_lineadapter(base_dir, repo_dir)
-        gradle = PluginGradle.new(base_dir)
-        gradle.jar_files.concat Pathname.glob(repo_dir/'*.jar')
-        gradle.jni_dirs.push repo_dir/'libs'
-        gradle
-    end
+    attr_accessor :jar_files, :jni_dirs
 
-    attr_accessor :base_dir, :jar_files, :jni_dirs
-
-    def initialize(base_dir)
-        @base_dir = base_dir
+    def initialize
         @jar_files = []
         @jni_dirs = []
     end
 
-    def write
-        target_file = 'plugin.gradle'
+    def write(target_file, base_dir = nil)
+        base_dir ||= target_file.dirname
+        mk_path = lambda { |p|
+            p.relative_path_from base_dir
+        }
 
         files_line = @jar_files.map { |x|
             "'#{mk_path(x)}'"
@@ -33,7 +28,7 @@ class PluginGradle
                     main.jniLibs {
             EOF
             dst.puts @jni_dirs.map { |x|
-            "            srcDirs += '#{mk_path(x)}'"
+            "            srcDirs += '#{mk_path.call(x)}'"
             }
             dst.puts <<~EOF
                     }
@@ -41,9 +36,5 @@ class PluginGradle
             }
             EOF
         }
-    end
-
-    def mk_path(p)
-        p.relative_path_from @base_dir
     end
 end
