70491dce02b2c4ed6613ad0de9fdbb6befc055a9
Kunio SAWATANI <fsawa@fathens.org>
2017-01-19 05:45:58 +0000

    一時ファイルに書き込まず、バッファに編集をためて最後に書き込む

--- platforms/android/hooks/after_plugin_install.rb
+++ platforms/android/hooks/after_plugin_install.rb
@@ -2,41 +2,40 @@
 
 require 'pathname'
 
-def rewrite_gradle(file_src)
-    file_dst = "#{file_src}.tmp"
-
+def rewrite_gradle(file)
     compile = true
     srcdirs = true
     plugin = true
     classpath = true
 
-    open(file_src, 'r') { |src|
-        open(file_dst, 'w') { |dst|
-            src.each_line { |line|
-                dst.puts line
-                add_line = lambda { |text|
-                    dst.puts "#{line.match(/^[\s]*/)[0]}#{text}"
-                }
-                if compile && line =~ /^\s*compile\s/
-                    add_line.call "compile 'org.jetbrains.kotlin:kotlin-stdlib:1.+'"
-                    compile = false
-                end
-                if srcdirs && line =~ /java\.srcDirs/
-                    add_line.call "kotlin.srcDirs = ['kotlin']"
-                    srcdirs = false
-                end
-                if plugin && line =~ /apply plugin:/
-                    add_line.call "apply plugin: 'kotlin-android'"
-                    plugin = false
-                end
-                if classpath && line =~ /classpath 'com\.android\.tools\.build:gradle:[1-2]\.[1-9]\./
-                    add_line.call "classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.+'"
-                    classpath = false
-                end
-            }
+    lines = []
+    open(file, 'r') { |src|
+        src.each_line { |line|
+            lines.push line
+            add_line = lambda do |text|
+                lines.push "#{line.match(/^[\s]*/)[0]}#{text}"
+            end
+            if compile && line =~ /^\s*compile\s/
+                add_line["compile 'org.jetbrains.kotlin:kotlin-stdlib:1.+'"]
+                compile = false
+            end
+            if srcdirs && line =~ /java\.srcDirs/
+                add_line["kotlin.srcDirs = ['kotlin']"]
+                srcdirs = false
+            end
+            if plugin && line =~ /apply plugin:/
+                add_line["apply plugin: 'kotlin-android'"]
+                plugin = false
+            end
+            if classpath && line =~ /classpath 'com\.android\.tools\.build:gradle:[1-2]\.[1-9]\./
+                add_line["classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.+'"]
+                classpath = false
+            end
         }
     }
-    File.rename(file_dst, file_src)
+    open(file, 'w') { |dst|
+        dst.puts lines
+    }
 end
 
 $PROJECT_DIR = Pathname.pwd.realpath
